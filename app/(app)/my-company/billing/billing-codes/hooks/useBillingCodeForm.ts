9ebac5a3356cc73532d489727a7570d2ef4c7d19
Frank Hernandez <frank.hernandez@coderland.tech>
2026-04-24 15:19:54 -0600

    billin codes fix

--- app/(app)/my-company/billing/billing-codes/hooks/useBillingCodeForm.ts
+++ app/(app)/my-company/billing/billing-codes/hooks/useBillingCodeForm.ts
@@ -85,13 +85,13 @@ export function useBillingCodeForm({ billingCodeId = null }: UseBillingCodeFormP
         const type = searchParams.get("type") as "CPT" | "HCPCS" | null
         const code = searchParams.get("code")
         const description = searchParams.get("description")
-        const modifier = searchParams.get("modifier")
+        const modifier = searchParams.get("modifier") || searchParams.get("modifiers")
         
-        if (type && code && description) {
+        if (type && code) {
           form.reset({
             type,
             code,
-            description,
+            description: description || "",
             modifiers: modifier || "",
             parent: "",
             placeServiceId: "",
